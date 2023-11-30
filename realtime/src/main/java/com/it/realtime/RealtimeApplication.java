823245326e571019942eebd25e05de76cace6c7a
nagsai-nani <133091211+nagsai-nani@users.noreply.github.com>
2023-11-30 12:10:28 +0530

    Initial commit

---
+++ realtime/src/main/java/com/it/realtime/RealtimeApplication.java
@@ -0,0 +1,13 @@
+package com.it.realtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeApplication.class, args);
+	}
+
+}
